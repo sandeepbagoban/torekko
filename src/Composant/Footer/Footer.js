dc0d09d20fc19a4fecdc34f92f9dafd3bd5a06fc
Balthi <balthazar.roque@epitech.eu>
2021-10-08 11:52:29 +0200

    [ADD] new footer with social media part

--- src/Composant/Footer/Footer.js
+++ src/Composant/Footer/Footer.js
@@ -1,6 +1,11 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import mainLogo from '../../Illustration/logo-torreko.png'
+import Discord from '../../Illustration/Icon/discorde.png'
+import Telegram from '../../Illustration/Icon/telegram.png'
+import Linkedin from '../../Illustration/Icon/linkedin.png'
+import Instagram from '../../Illustration/Icon/instagram.png'
+import Twitter from '../../Illustration/Icon/twitter.png'
 import './Footer.css'
 
 import { Trans, useTranslation } from 'react-i18next'
@@ -8,24 +13,54 @@ import { Trans, useTranslation } from 'react-i18next'
 
 
 
-function Footer(){
+function Footer() {
 
     const { t, i18n } = useTranslation()
 
 
-    return(
-    <footer class="footer">
-        <div class="footer__addr">
-            <h1 class="footer__logo">TOREKKO</h1>
-                
-            <h2>Contact</h2>
-            
-            <address>
-            <a class="footer__btn" href="mailto:contact@torekko.com">contact@torekko.com</a>
-            </address>
-        </div>
-        
-    <ul class="footer__nav">
+    return (
+        <footer class="footer">
+            <div class="footer__addr">
+                <h1 class="footer__logo">TOREKKO</h1>
+
+                <h2>Contact</h2>
+
+                <address>
+                    <a class="footer__btn" href="mailto:contact@torekko.com">contact@torekko.com</a>
+                </address>
+            </div>
+
+            <ul class="footer__nav">
+                <li class="nav__item">
+                    <a className="socialmedia" href="https://www.instagram.com/torekkocom/"><img className="socialmedia_logo" src={Instagram} alt="Instagram"/></a>
+                    <h5 class="socialmedia_text">Instagram</h5>
+                </li>
+
+                <li class="nav__item">
+                    <a className="socialmedia" href="https://twitter.com/torekkocom?s=20"><img className="socialmedia_logo" src={Twitter} alt="Instagram" /></a>
+                    <h5 class="socialmedia_text">Twitter</h5>
+                </li>
+
+                <li class="nav__item">
+                    <a className="socialmedia" href="https://www.linkedin.com/company/torekko/"><img className="socialmedia_logo" src={Linkedin} alt="Instagram" /></a>
+                    <h5 class="socialmedia_text">Linkedin</h5>
+                </li>
+
+                <li class="nav__item">
+                    <a className="socialmedia" href="https://t.me/torekko"><img className="socialmedia_logo" src={Telegram} alt="Instagram" /></a>
+                    <h5 class="socialmedia_text">Community</h5>
+                </li>
+                <li class="nav__item">
+                    <a className="socialmedia" href="https://t.me/torekkoann"><img className="socialmedia_logo" src={Telegram} alt="Telegram" /></a>
+                    <h5 class="socialmedia_text">Announcements</h5>
+                </li>
+                <li class="nav__item">
+                    <a className="socialmedia" href="https://discord.gg/Tnwtu5YjDA"><img className="socialmedia_logo" src={Discord} alt="Discord" /></a>
+                    <h5 class="socialmedia_text">Discord</h5>
+                </li>
+            </ul>
+
+            {/* <ul class="footer__nav">
         <li class="nav__item">
         <h2 class="nav__title"><Trans>footer.project</Trans></h2>
 
@@ -81,16 +116,16 @@ function Footer(){
         </li>
         
         
-    </ul>
-    
-    <div class="legal">
-        <p>&copy; 2021 TORREKO. All rights reserved.</p>
-        
-        <div class="legal__links">
-        <a href="#">CGV - Mentions légales</a>
-        </div>
-    </div>
-    </footer>
+    </ul> */}
+
+            <div class="legal">
+                <p>&copy; 2021 TORREKO. All rights reserved.</p>
+
+                <div class="legal__links">
+                    <a href="#">CGV - Mentions légales</a>
+                </div>
+            </div>
+        </footer >
     )
 }
 
