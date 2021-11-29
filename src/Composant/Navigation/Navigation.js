d02650fe82760c483cf332dfceed9b81482ce711
Thomas <thomas.itel@epitech.eu>
2021-11-29 22:39:05 +0100

    [ADD] button vesting

--- src/Composant/Navigation/Navigation.js
+++ src/Composant/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React, {useState, Link} from 'react'
+import React, { useState, Link } from 'react'
 
 import ReactCountryFlag from "react-country-flag"
 import { GiHamburgerMenu } from "@react-icons/all-files/gi/GiHamburgerMenu";
@@ -19,82 +19,86 @@ const Navigation = () => {
 
   //Element de la bar de menu
   const MenuValues = [
-    { id: 1, text: t('navigation.discover-torekko'), link: "#discover"},
-    { id: 2, text: t('navigation.attribut'), link: "#attribut"},
-    { id: 3, text: t('navigation.booster-drop'), link: "#booster-drop"},
-    { id: 4, text: t('navigation.roadmap'), link: "#roadmap"},
-    { id: 5, text: t('navigation.investor'), link: "#investor"},
+    { id: 1, text: t('navigation.discover-torekko'), link: "#discover" },
+    { id: 2, text: t('navigation.attribut'), link: "#attribut" },
+    { id: 3, text: t('navigation.booster-drop'), link: "#booster-drop" },
+    { id: 4, text: t('navigation.roadmap'), link: "#roadmap" },
+    { id: 5, text: t('navigation.investor'), link: "#investor" },
   ];
 
   //Afficher la class active sur le menu
   const [activeId, setActiveId] = useState();
   const scrollY = useScrollPosition(60 /*fps*/)
 
-  
 
-  return(
-      <nav class={scrollY === 0 ? "navigation navbar navbar-expand-md w-100" : "navigation navbar navbar-expand-md w-100 navScroll"}>
+
+  return (
+    <nav class={scrollY === 0 ? "navigation navbar navbar-expand-md w-100" : "navigation navbar navbar-expand-md w-100 navScroll"}>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-          <ul class="navbar-nav mr-auto">
-              <a className="nav-link" href="#"><img className="logo" src={require('../../Illustration/logo-torreko.png').default} alt="logo"/></a>            
-          </ul>
+        <ul class="navbar-nav mr-auto">
+          <a className="nav-link" href="#"><img className="logo" src={require('../../Illustration/logo-torreko.png').default} alt="logo" /></a>
+        </ul>
       </div>
       <div class="mx-auto order-0 navbar-collapse collapse dual-collapse2">
-          <ul class="navbar-nav mr-auto">
-                {MenuValues.map((val) => (
-                  <li className={activeId === val.id ? " nav-item" : "nav-item"} onClick={() => setActiveId(val.id)}>
-                    <a href={val.link} id={val.link} className="navbar-link nav-link text-nowrap">{val.text}</a>
-                  </li>
-                ))}
+        <ul class="navbar-nav mr-auto">
+          {MenuValues.map((val) => (
+            <li className={activeId === val.id ? " nav-item" : "nav-item"} onClick={() => setActiveId(val.id)}>
+              <a href={val.link} id={val.link} className="navbar-link nav-link text-nowrap">{val.text}</a>
+            </li>
+          ))}
 
-                <li className="nav-item">
-                  <a href="https://medium.com/@torekko" className="animated" target="_blank">
-                    <span className="span1">M</span>
-                    <span className="span2">e</span>
-                    <span className="span3">d</span>
-                    <span className="span4">i</span>
-                    <span className="span5">u</span>
-                    <span className="span6">m</span>
-                  </a>
-                </li>
-              
-          </ul>
+          <li className="nav-item">
+            <a href="https://vesting.torekko.com/" className="navbar-link nav-link text-nowrap">
+              Vesting
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="https://medium.com/@torekko" className="animated" target="_blank">
+              <span className="span1">M</span>
+              <span className="span2">e</span>
+              <span className="span3">d</span>
+              <span className="span4">i</span>
+              <span className="span5">u</span>
+              <span className="span6">m</span>
+            </a>
+          </li>
+        </ul>
       </div>
       <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-          <ul class="navbar-nav ml-auto flags-icons text-nowrap">
-              <li class="nav-item">
-                <ReactCountryFlag
-                    className="flag-icon"
-                    countryCode="FR"
-                    aria-label="France"
-                    onClick={()=>i18next.changeLanguage('fr')}
-                />
-              </li>
-              <li class="nav-item">
-                  <ReactCountryFlag
-                    className="flag-icon"
-                    countryCode="US"
-                    aria-label="United States"
-                    onClick={()=>i18next.changeLanguage('us')}
-                  />
-              </li>
-              <li class="nav-item">
-                  <ReactCountryFlag
-                    className="flag-icon"
-                    countryCode="JP"
-                    aria-label="Japan"
-                    onClick={()=>i18next.changeLanguage('jp')}
-                  />
-              </li>
-          </ul>
+        <ul class="navbar-nav ml-auto flags-icons text-nowrap">
+          <li class="nav-item">
+            <ReactCountryFlag
+              className="flag-icon"
+              countryCode="FR"
+              aria-label="France"
+              onClick={() => i18next.changeLanguage('fr')}
+            />
+          </li>
+          <li class="nav-item">
+            <ReactCountryFlag
+              className="flag-icon"
+              countryCode="US"
+              aria-label="United States"
+              onClick={() => i18next.changeLanguage('us')}
+            />
+          </li>
+          <li class="nav-item">
+            <ReactCountryFlag
+              className="flag-icon"
+              countryCode="JP"
+              aria-label="Japan"
+              onClick={() => i18next.changeLanguage('jp')}
+            />
+          </li>
+        </ul>
       </div>
-  </nav>
-  )   
+    </nav>
+  )
 }
-          
+
 
 
 
